e347268b99eabedbd8f2fb80b79eeedec1cf204e
Eric.A <arnhemeric60@gmail.com>
2017-04-07 15:48:27 +0200

    changement du script du remove + ajout couleur blanche à la croix dans les modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,39 @@
 var coffeeApp = angular.module('coffeeApp', []);
 
-coffeeApp.controller('cartCtrl', function($scope){
-    $scope.count = 1;
+coffeeApp.controller('cartCtrl', function ($scope) {
+    $scope.quantity = 1;
     // Fonction qui se déclanche quand on clique sur le bouton "+"
-    $scope.countPlus = function() {
+    $scope.quantityPlus = function () {
         // Si la quantité est supérieur ou égale à 1
-        if($scope.count >= 1){
+        if ($scope.quantity >= 1) {
             // On peut ajouter 1 à la quantité
-            $scope.count = $scope.count + 1;
+            $scope.quantity = $scope.quantity + 1;
         }
     };
+
     // Fonction qui se déclanche quand on clique sur le bouton "-"
-    $scope.countMinus = function() {
+    $scope.quantityMinus = function () {
         // Si la quantité est supérieur à 1
-        if($scope.count > 1){
+        if ($scope.quantity > 1) {
             // On peut enlever 1 à la quantité
-            $scope.count = $scope.count - 1;
+            $scope.quantity = $scope.quantity - 1;
         }
     };
+    
+        // Fonction qui supprime l'item du panier au clic sur le bouton Supprimer
+        $scope.remove = function ($index) {
+            $scope.coffees.splice($index, 1);
+        };
+
 });
+
+
+// Ici il faut['$http', '$scope', avant la fonction sinon ça ne marche pas
+coffeeApp.controller('coffeeCtrl', ['$http', '$scope', function ($http, $scope) {
+        //$http.get appel le fichier .json
+        $http.get('assets/js/coffee.json')
+                .then(function (reponse) {
+                    //reponse.data renvoye ce qui est demandé
+                    $scope.coffees = reponse.data;
+                });
+    }]);
