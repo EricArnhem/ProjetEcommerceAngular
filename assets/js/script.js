f3c9cedcacdf3ad204cd2a5c6164852429c3eb4d
Eric.A <arnhemeric60@gmail.com>
2017-04-10 11:08:46 +0200

    ajout des fonctions pour les quantités + total prix d'un objet + total global des objets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,31 @@
 var coffeeApp = angular.module('coffeeApp', []);
 
 coffeeApp.controller('cartCtrl', function ($scope) {
-    $scope.quantity = 1;
     // Fonction qui se déclanche quand on clique sur le bouton "+"
-    $scope.quantityPlus = function () {
-        // Si la quantité est supérieur ou égale à 1
-        if ($scope.quantity >= 1) {
-            // On peut ajouter 1 à la quantité
-            $scope.quantity = $scope.quantity + 1;
-        }
+    $scope.quantityPlus = function (cartItem) {
+        cartItem.quantity++;
     };
 
     // Fonction qui se déclanche quand on clique sur le bouton "-"
-    $scope.quantityMinus = function () {
-        // Si la quantité est supérieur à 1
-        if ($scope.quantity > 1) {
-            // On peut enlever 1 à la quantité
-            $scope.quantity = $scope.quantity - 1;
+    $scope.quantityMinus = function (cartItem) {
+        if (cartItem.quantity > 0) {
+            cartItem.quantity--;
         }
     };
-    
-        // Fonction qui supprime l'item du panier au clic sur le bouton Supprimer
-        $scope.remove = function ($index) {
-            $scope.cartItems.splice($index, 1);
-        };
+
+    $scope.totalPrice = function () {
+        var total = 0;
+        angular.forEach($scope.cartItems, function (cartItem, index) {
+            total += cartItem.prix * cartItem.quantity;
+        });
+        console.log(total);
+        return total;
+    };
+
+    // Fonction qui supprime l'item du panier au clic sur le bouton Supprimer
+    $scope.remove = function ($index) {
+        $scope.cartItems.splice($index, 1);
+    };
 
 });
 
@@ -37,15 +39,15 @@ coffeeApp.controller('coffeeCtrl', ['$http', '$scope', function ($http, $scope) 
                     $scope.coffees = reponse.data;
                     $scope.orderByPrix = "prix";
                 });
-                
+
         $http.get('assets/js/cart.json')
                 .then(function (reponse) {
                     // reponse.data renvoie ce qui est demandé
                     $scope.cartItems = reponse.data;
                 });
-                
+
         // Fonction pour ajouter un item
         $scope.addItem = function () {
-            $scope.cartItems.push({"img": "dolcegusto.jpg", "marque": "Nescafe", "reference": "m001", "type": "machine", "nom": "Dolce Gusto", "description": "Système haute pression (jusqu’à 15 bars) associé aux capsules brevetées Nescafé", "prix": 129.99});
+            $scope.cartItems.push({"img": this.coffee.img, "marque": this.coffee.marque, "reference": this.coffee.reference, "type": this.coffee.type, "nom": this.coffee.nom, "description": this.coffee.description, "prix": this.coffee.prix, "quantity": this.coffee.quantity});
         };
     }]);
